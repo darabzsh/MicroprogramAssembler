75c98a219363391d76face9db5850e4eabc4b35e
HRBag <eng.hrbag@gmail.com>
2023-06-23 01:09:25 +0330

    Add MicroProgram Label

--- Assembler/mainwindow.cpp
+++ Assembler/mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QDebug>
+#include <QMessageBox>
+
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -31,10 +35,66 @@ MainWindow::MainWindow(QWidget *parent)
     ui->Main_Memory->resizeRowsToContents();
     ui->Main_Memory->resizeColumnsToContents();
 //    ui->Main_Memory-
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_MicroButton_clicked()
+{
+    QString micro = ui->microprogram->toPlainText();
+    QStringList lines = micro.split("\n",Qt::SkipEmptyParts);
+    QList<QStringList> wordList;
+
+    foreach (const QString& line, lines) {
+        QStringList lineWords = line.split(" ", Qt::SkipEmptyParts);
+        wordList.append(lineWords);
+    }
+//    wordList.removeAll([](const QStringList& list) {
+//        return list.isEmpty();
+//    });
+
+    //Conditions
+
+    int count_end = 0;
+    foreach (const QStringList& words, wordList) {
+        count_end += words.count("END");
+    }
+    if(count_end != 1){
+        QMessageBox::critical(nullptr, "Error", "MicroProgram just need one <END>...");
+        return ;
+    }
+    // --------
+    int currentline = 0;
+
+//    qDebug() << wordList[0][1] ;
+
+    ui->microprogram->clear();
+    QStringList newline;
+    foreach (const QStringList& words, wordList) {
+        QString line = words.join(" ");
+        newline.append(line);
+    }
+
+    ui->microprogram->append(newline.join("\n"));
+
+    foreach (const QStringList& words, wordList) {
+        if (words.at(0) == "END")
+            break;
+        if (words.at(0) == "ORG")
+            currentline = words.at(1).toInt();
+        if (words.at(0).endsWith(":")){
+            qDebug() << words.at(0) ;
+            QString text = words.at(0);
+            text = text.chopped(1);
+            ui->Microprogram_Memory->setItem(currentline-1,2,new QTableWidgetItem(text));
+        }
+        currentline ++;
+    }
+
 }
 
